015bf40c3a7f573ff3e22fd6cdf8e4be868bb8d6
Bryan Melo <bmelo650@gmail.com>
2024-01-23 22:10:33 -0800

    LeetCode Problem #1342

--- Bitwise/1342.Number_of_Steps_to_Reduce_a_Number_to_Zero.py
+++ Bitwise/1342.Number_of_Steps_to_Reduce_a_Number_to_Zero.py
@@ -1,9 +1,9 @@
 # Problem Statement
 """ 
-Given an integer num, return the number of steps to reduce it to zero.
+Given an integer: num, return the number of steps to reduce it to zero.
 
 In one step, if the current number is even, you have to divide it by 2, 
-otherwise, you have to subtract 1 from it.
+otherwise, you have to subtract 1 from it. 
 
  
 Example 1:
