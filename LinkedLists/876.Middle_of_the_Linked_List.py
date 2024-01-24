e285aa02d6ff66d2fabbf792ee35156737f2ec36
Bryan Melo <bmelo650@gmail.com>
2024-01-23 22:58:57 -0800

    LeetCode Problem #383 Created

--- LinkedLists/876.Middle_of_the_Linked_List.py
+++ LinkedLists/876.Middle_of_the_Linked_List.py
@@ -1,4 +1,4 @@
-# Approach
+# Problem Statement
 """ 
 Given the head of a singly linked list, return the middle node of the linked list.
 
---
+++ Strings/383.Ransom_Note.py
@@ -0,0 +1,37 @@
+# Problem Statement
+""" 
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed 
+by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+Example 1:
+    Input: ransomNote = "a", magazine = "b"
+    Output: false
+
+Example 2:
+    Input: ransomNote = "aa", magazine = "ab"
+    Output: false
+"""
+
+
+# Approach
+"""
+
+"""
+
+
+# Time Complexity: 
+# Space Complexity 
+
+
+# Code
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        pass
+    
+    
+# Example usage
+if __name__ == "__main__":  
+    # Creating a Solution instance 
+    solution = Solution()
