ed8942db8aa932008d3a835b722cef9e36a9ea3d
Bryan Melo <bmelo650@gmail.com>
2024-01-23 22:02:52 -0800

    Worked on LeetCode #876

--- LinkedLists/876.Middle_of_the_Linked_List.py
+++ LinkedLists/876.Middle_of_the_Linked_List.py
@@ -8,12 +8,23 @@ If there are two middle nodes, return the second middle node.
 
 # Approach
 """ 
-
+    - Initialize a variable (count) that will store the number of nodes in linked list
+    - Initiliaze another variable (check) that will be used to compare to count
+    - Initialize an empty node (new_head)
+    - Save the head of linked list
+    - Iterate through linked list
+        * Increment count if node exist
+    - Divide count by 2
+    - Iterate though linked list
+        * when check == count, assign node to new_head
+        * keep iterating through linked list until None is reached, add each node to new_linked_list
+    - Return new_linked_list
+    
 """
 
 
-# Time Complexity:
-# Space Complexity
+# Time Complexity: O(N)
+# Space Complexity O(N)
 
 
 # Code
@@ -29,19 +40,51 @@ class ListNode:
 
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        new_list = list()
+        temp = head
+        check = 0
         
-        while head != None:
-            new_list.append(head.val)
+        count = Solution.countNodes(temp)
+        count = count / 2
+        
+        print(count)
+        return temp
+        
+        
+        
     
+    def countNodes(self, head: Optional[ListNode]) -> int:
+        count = 0
+        
+        while head:
+            count += 1
+            head = head.next
+
+        return count        
+        
 
 # Example usage
 if __name__ == "__main__":  
     # Creating a Solution instance 
     solution = Solution()
     
-    # Create Linked list
-    head = ListNode(1)
+    # Create Nodes
+    node1 = ListNode(1)
+    node2 = ListNode(2)
+    node3 = ListNode(3)
+    node4 = ListNode(4)
+    node5 = ListNode(5)
+    
+    # Create Linked-List
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
     
     # Printing results    
+    head = solution.middleNode(node1)
     
+    current = head
+    
+    while current:
+        print(current.val)
+        current = current.next
